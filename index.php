0966198a49e814d018f2751ae824da0b750955f6
abhiyaan@yahoo.com <abhiyaan@yahoo.com>
2019-01-20 09:02:19 -0800

    first posts

--- index.php
+++ index.php
@@ -4,5 +4,23 @@
 <?php if(have_posts()) { ?>
     <?php while(have_posts()) { ?>
         <?php the_post(); ?>
-
+        <h2>
+            <a href="<?php the_permalink() ?>" title="<?php the_title_attribute() ?>">
+                <?php echo get_the_title(); ?>
+            </a>
+        </h2>
+        <div>
+            Posted on:
+                <a href="<?php the_permalink() ?>">
+                   <time datetime="<?php echo the_date
+                                   ?>"></time>
+                    
+                </a>
+        </div>
+        <div>
+            <?php the_excerpt() ?>
+        </div>
+    <?php } ?>    
+<?php } else ?>
+    <?php '<p> Sorry I yo have no posts </p>'  ?>      
 <?php get_footer(); ?>
